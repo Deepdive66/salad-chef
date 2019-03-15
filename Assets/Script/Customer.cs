94522997112eed5236910631798349dca5b8548d
Darshan kawar <deeptibelsare@Deeptis-MacBook-Air.local>
2019-03-15 09:01:24 +0530

    First commit

---
+++ Assets/Script/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Customer : MonoBehaviour {
+
+   // Rigidbody2D rb;
+    public static float WaitTime;
+
+	void Start () {
+		
+        WaitTime = 10;
+      //  rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        WaitTime -= 1f;
+
+        if (WaitTime <= 0){
+            // Player looses point 
+        }
+		
+	}
+}
---
+++ Assets/Script/TimerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour {
+
+    Image timerBar;
+    public float maxTime = 5f;
+    float timeLeft;
+
+	void Start () {
+
+        timerBar = GetComponent<Image>();
+        timeLeft = maxTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+            timerBar.fillAmount = timeLeft / maxTime;
+        }
+        else
+        {
+            Time.timeScale = 0;
+        }
+	}
+}
